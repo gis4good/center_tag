07fc01350a1f5123916a5e37635192c0aebd1265
gis4good <105337168+gis4good@users.noreply.github.com>
2023-12-31 09:15:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -123,12 +123,12 @@ def centertag():
         dev=request.form.get('deviceInfo')   
         tim=request.form.get('timestamp')       
         response_data = {
-           'Emp_id':empid.replace("Employee ID - ",''), 
-           'Branch':bid.replace("Branch - ",''),
-           'State': state.replace("State - ",''),
-           'District':district.replace("District - ",''),
-           'Tehsil':tehsil.replace("Tehsil - ",''),
-           'zone': zone.replace("Zone - ",''),
+           'Emp_id':''.join(empid).replace("Employee ID - ",''), 
+           'Branch':''.join(bid).replace("Branch - ",''),
+           'State': ''.join(state).replace("State - ",''),
+           'District':''.join(district).replace("District - ",''),
+           'Tehsil':''.join(tehsil).replace("Tehsil - ",''),
+           'zone': ''.join(zone).replace("Zone - ",''),
            'Village':vila,
            'Loan_id':lid,
            'Center_name':cntrnm,
